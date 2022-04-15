9da2b65ac86e48c19682639b79b13fb677dbaf2d
Mark Canlas <github@htmlism.com>
2022-04-14 21:32:51 -0400

    added database

--- src/main/scala/com/htmlism/temporaldiagrams/PlantUml.scala
+++ src/main/scala/com/htmlism/temporaldiagrams/PlantUml.scala
@@ -19,6 +19,9 @@ object PlantUml {
 
       case Queue(name, tag) =>
         s"queue $name" + tag.fold("")(s => s" << $s >>")
+
+      case Database(name, tag) =>
+        s"database $name" + tag.fold("")(s => s" << $s >>")
     }
 }
 
@@ -29,3 +32,5 @@ case class Component(name: String, tag: Option[String]) extends PlantUml
 case class Link(src: String, dest: String) extends PlantUml
 
 case class Queue(name: String, tag: Option[String]) extends PlantUml
+
+case class Database(name: String, tag: Option[String]) extends PlantUml
