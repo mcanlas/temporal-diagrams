d7d9efd2215df9d85d52d3623c884a629624b058
Mark Canlas <github@htmlism.com>
2022-11-27 12:17:04 -0500

    nominal namepsace support

--- temporal-diagrams-server/src/main/scala/com/htmlism/temporaldiagrams/server/KeyValueDecoder.scala
+++ temporal-diagrams-server/src/main/scala/com/htmlism/temporaldiagrams/server/KeyValueDecoder.scala
@@ -4,8 +4,17 @@ import cats._
 import cats.data._
 import cats.syntax.all._
 
-sealed trait KeyValueDecoder[A] {
-  def decode(xs: Map[String, List[String]]): ValidatedNec[String, A]
+// TODO should be a codec actually...
+sealed trait KeyValueDecoder[A] { self: KeyValueDecoder[A] =>
+  def decode(xs: Map[String, List[String]], ns: List[String]): ValidatedNec[String, A]
+
+  // TODO thread in the namespace into the errors, maybe changing the signature of `decode`
+  def withNamespace(n: String): KeyValueDecoder[A] =
+    new KeyValueDecoder[A] {
+      def decode(xs: Map[String, List[String]], ns: List[String]): ValidatedNec[String, A] =
+        self
+          .decode(xs.view.filterKeys(_.startsWith(n)).toMap, n :: ns)
+    }
 }
 
 object KeyValueDecoder {
@@ -13,10 +22,10 @@ object KeyValueDecoder {
     new Semigroupal[KeyValueDecoder] {
       def product[A, B](fa: KeyValueDecoder[A], fb: KeyValueDecoder[B]): KeyValueDecoder[(A, B)] =
         new KeyValueDecoder[(A, B)] {
-          def decode(xs: Map[String, List[String]]): ValidatedNec[String, (A, B)] =
-            fa.decode(xs) match {
+          def decode(xs: Map[String, List[String]], ns: List[String]): ValidatedNec[String, (A, B)] =
+            fa.decode(xs, ns) match {
               case Validated.Valid(x) =>
-                fb.decode(xs) match {
+                fb.decode(xs, ns) match {
                   case Validated.Valid(y) =>
                     (x, y).valid
                   case Validated.Invalid(err) =>
@@ -24,7 +33,7 @@ object KeyValueDecoder {
                 }
 
               case Validated.Invalid(erry) =>
-                fb.decode(xs) match {
+                fb.decode(xs, ns) match {
                   case Validated.Valid(_) =>
                     erry.invalid
                   case Validated.Invalid(errx) =>
@@ -35,16 +44,16 @@ object KeyValueDecoder {
     }
 
   case class Value[A](key: String)(implicit A: ValueDecoder[A]) extends KeyValueDecoder[A] {
-    def decode(xs: Map[String, List[String]]): ValidatedNec[String, A] =
+    def decode(xs: Map[String, List[String]], ns: List[String]): ValidatedNec[String, A] =
       Either
-        .fromOption(xs.get(key), s"key $key did not exist")
+        .fromOption(xs.get(ns.mkString("") + key), s"key $key did not exist")
         .map(xs => xs.headOption.getOrElse(s"available key $key had no values"))
         .flatMap(A.decode)
         .toValidatedNec
   }
 
   case class Record[A](namespace: String)(implicit A: RecordDecoder[A]) extends KeyValueDecoder[A] {
-    def decode(xs: Map[String, List[String]]): ValidatedNec[String, A] =
+    def decode(xs: Map[String, List[String]], ns: List[String]): ValidatedNec[String, A] =
       A.decode(xs)
   }
 }
