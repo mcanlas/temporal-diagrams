b2281c6fbcbbcdfd04bd13d0383abf4b3c991df9
Mark Canlas <github@htmlism.com>
2023-04-15 14:30:55 -0400

    weaver 0.8.3

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -43,7 +43,7 @@ object ProjectPlugin extends AutoPlugin {
 
       def withTesting: Project = {
         val weaverVersion =
-          "0.8.1"
+          "0.8.3"
 
         p.settings(
           testFrameworks += new TestFramework("weaver.framework.CatsEffect"),
