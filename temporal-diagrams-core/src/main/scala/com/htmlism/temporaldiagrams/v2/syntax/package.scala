10fce2c95df4db249b31a5cd934304cbe44d3cdc
Mark Canlas <github@htmlism.com>
2023-09-16 23:37:30 +0200

    document and localize d

--- temporal-diagrams-core/src/main/scala/com/htmlism/temporaldiagrams/v2/syntax/package.scala
+++ temporal-diagrams-core/src/main/scala/com/htmlism/temporaldiagrams/v2/syntax/package.scala
@@ -1,8 +1,24 @@
 package com.htmlism.temporaldiagrams.v2
 
 package object syntax {
-  implicit class RenderableOps[D, A](x: A) {
-    def r(implicit enc: HighlightEncoder[D, A]): Renderable[D] =
+
+  /**
+    * Postfix syntax enhancement for marking an expression as renderable
+    *
+    * @param x
+    *   The data being rendered
+    * @tparam A
+    *   The type of the data being rendered
+    */
+  implicit class RenderableOps[A](x: A) {
+
+    /**
+      * Marks the expression as renderable to `D`, without any tags
+      *
+      * @tparam D
+      *   The target diagram language
+      */
+    def r[D](implicit enc: HighlightEncoder[D, A]): Renderable[D] =
       RenderableA(x)
   }
 }
