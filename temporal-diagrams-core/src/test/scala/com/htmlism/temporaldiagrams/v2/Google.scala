026eb00e1a4470ab15ba1294a4b36617718c0576
Mark Canlas <github@htmlism.com>
2023-09-19 03:48:58 -0400

    directly use nel encoder

--- temporal-diagrams-core/src/test/scala/com/htmlism/temporaldiagrams/v2/Google.scala
+++ temporal-diagrams-core/src/test/scala/com/htmlism/temporaldiagrams/v2/Google.scala
@@ -1,18 +1,21 @@
 package com.htmlism.temporaldiagrams.v2
 
+import cats.data.NonEmptyList
+
 import com.htmlism.temporaldiagrams.v2.ToyDiagramLanguage._
 
 object Google {
   case class Compute(s: String)
 
   object Compute {
-    implicit val computeEncoder: HighlightEncoder[ToyDiagramLanguage, Compute] =
-      new HighlightEncoder[ToyDiagramLanguage, Compute] {
-        def encode(x: Compute): ToyDiagramLanguage =
-          Component(s"google compute: ${x.s}")
+    // differs from amazon encoder, which is not nel
+    implicit val computeNelEncoder: HighlightEncoder[NonEmptyList[ToyDiagramLanguage], Compute] =
+      new HighlightEncoder[NonEmptyList[ToyDiagramLanguage], Compute] {
+        def encode(x: Compute): NonEmptyList[ToyDiagramLanguage] =
+          NonEmptyList.of(Component(s"google compute: ${x.s}"))
 
-        def encodeWithHighlights(x: Compute, highlighted: Boolean): ToyDiagramLanguage =
-          Component(s"google compute: ${x.s} ${highlighted.toString}")
+        def encodeWithHighlights(x: Compute, highlighted: Boolean): NonEmptyList[ToyDiagramLanguage] =
+          NonEmptyList.of(Component(s"google compute: ${x.s} ${highlighted.toString}"))
       }
   }
 }
