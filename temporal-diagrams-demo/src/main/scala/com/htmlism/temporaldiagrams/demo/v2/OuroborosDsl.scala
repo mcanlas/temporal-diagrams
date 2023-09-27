e6c428425b23ce8099c91164b376b2140bc892fd
Mark Canlas <github@htmlism.com>
2023-09-27 14:15:56 -0400

    with encoders

--- temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/OuroborosDsl.scala
+++ temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/OuroborosDsl.scala
@@ -5,7 +5,7 @@ sealed trait OuroborosDsl
 object OuroborosDsl {
   case class Type(name: String) extends OuroborosDsl
 
-  case class Encoding(src: String, dest: String) extends OuroborosDsl
+  case class Encoding(src: String, dest: String, name: String) extends OuroborosDsl
 
   case class Output(language: String) extends OuroborosDsl
 }
--- temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/WriteOuroborosDiagram.scala
+++ temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/WriteOuroborosDiagram.scala
@@ -12,7 +12,13 @@ object WriteOuroborosDiagram extends WriteOuroborosDiagram[IO] with IOApp.Simple
       OuroborosDsl.Type("TemporalDiagrams.PlantUml"),
       OuroborosDsl.Type("TemporalDiagrams.Mermaid"),
       OuroborosDsl.Output("Plantuml"),
-      OuroborosDsl.Output("Mermaid")
+      OuroborosDsl.Output("Mermaid"),
+      OuroborosDsl.Encoding("User.Config", "User.Dsl", "Episode 1"),
+      OuroborosDsl.Encoding("User.Config", "User.Dsl", "Episode 2"),
+      OuroborosDsl.Encoding("User.Dsl", "TemporalDiagrams.PlantUml", "HighlightEncoder[PlantUml, User.Dsl]"),
+      OuroborosDsl.Encoding("User.Dsl", "TemporalDiagrams.Mermaid", "HighlightEncoder[Mermaid, User.Dsl]"),
+      OuroborosDsl.Encoding("TemporalDiagrams.PlantUml", "Plantuml", "DiagramEncoder[Plantuml]"),
+      OuroborosDsl.Encoding("TemporalDiagrams.Mermaid", "Mermaid", "DiagramEncoder[Mermaid]")
     )
 }
 
