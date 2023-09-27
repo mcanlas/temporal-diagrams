20da4f947f77e2449fc59f7d022c6d899e8c508f
Mark Canlas <github@htmlism.com>
2023-09-27 14:38:36 -0400

    generate basic diagram

--- temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/OuroborosDsl.scala
+++ temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/OuroborosDsl.scala
@@ -1,5 +1,10 @@
 package com.htmlism.temporaldiagrams.demo.v2
 
+import cats.data.NonEmptyList
+
+import com.htmlism.temporaldiagrams.plantuml.PlantUml
+import com.htmlism.temporaldiagrams.v2.BrightEncoder
+
 sealed trait OuroborosDsl
 
 object OuroborosDsl {
@@ -8,4 +13,23 @@ object OuroborosDsl {
   case class Encoding(src: String, dest: String, name: String) extends OuroborosDsl
 
   case class Output(language: String) extends OuroborosDsl
+
+  implicit val demoBrightEncoder: BrightEncoder[NonEmptyList[PlantUml], OuroborosDsl] =
+    new BrightEncoder[NonEmptyList[PlantUml], OuroborosDsl] {
+      def encodeBrightly(x: OuroborosDsl, isBright: Boolean): NonEmptyList[PlantUml] =
+        x match {
+          case Type(s) =>
+            NonEmptyList
+              .one(PlantUml.Component(s, None, None))
+
+          case Encoding(src, dest, _) =>
+            // TODO need arrow comment support
+            NonEmptyList
+              .one(PlantUml.Arrow(src, dest))
+
+          case Output(s) =>
+            NonEmptyList
+              .one(PlantUml.Component(s, None, None))
+        }
+    }
 }
--- temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/WriteOuroborosDiagram.scala
+++ temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/WriteOuroborosDiagram.scala
@@ -1,28 +1,43 @@
 package com.htmlism.temporaldiagrams.demo.v2
 
-import cats.Applicative
+import scala.util.chaining._
+
 import cats.data.NonEmptyList
 import cats.effect._
+import cats.syntax.all._
 
-object WriteOuroborosDiagram extends WriteOuroborosDiagram[IO] with IOApp.Simple {
+import com.htmlism.temporaldiagrams.demo.FilePrinterAlg
+import com.htmlism.temporaldiagrams.plantuml.PlantUml
+import com.htmlism.temporaldiagrams.v2.Renderable
+import com.htmlism.temporaldiagrams.v2.syntax.RenderableOps
+
+object WriteOuroborosDiagram extends WriteOuroborosDiagram[IO](FilePrinterAlg[IO]) with IOApp.Simple {
   val diagram =
-    NonEmptyList.of(
-      OuroborosDsl.Type("User.Config"),
-      OuroborosDsl.Type("User.Dsl"),
-      OuroborosDsl.Type("TemporalDiagrams.PlantUml"),
-      OuroborosDsl.Type("TemporalDiagrams.Mermaid"),
-      OuroborosDsl.Output("Plantuml"),
-      OuroborosDsl.Output("Mermaid"),
-      OuroborosDsl.Encoding("User.Config", "User.Dsl", "Episode 1"),
-      OuroborosDsl.Encoding("User.Config", "User.Dsl", "Episode 2"),
-      OuroborosDsl.Encoding("User.Dsl", "TemporalDiagrams.PlantUml", "HighlightEncoder[PlantUml, User.Dsl]"),
-      OuroborosDsl.Encoding("User.Dsl", "TemporalDiagrams.Mermaid", "HighlightEncoder[Mermaid, User.Dsl]"),
-      OuroborosDsl.Encoding("TemporalDiagrams.PlantUml", "Plantuml", "DiagramEncoder[Plantuml]"),
-      OuroborosDsl.Encoding("TemporalDiagrams.Mermaid", "Mermaid", "DiagramEncoder[Mermaid]")
-    )
+    NonEmptyList
+      .of(
+        OuroborosDsl.Type("User.Config"),
+        OuroborosDsl.Type("User.Dsl"),
+        OuroborosDsl.Type("TemporalDiagrams.PlantUml"),
+        OuroborosDsl.Type("TemporalDiagrams.Mermaid"),
+        OuroborosDsl.Output("Plantuml"),
+        OuroborosDsl.Output("Mermaid"),
+        OuroborosDsl.Encoding("User.Config", "User.Dsl", "Episode 1"),
+        OuroborosDsl.Encoding("User.Config", "User.Dsl", "Episode 2"),
+        OuroborosDsl.Encoding("User.Dsl", "TemporalDiagrams.PlantUml", "HighlightEncoder[PlantUml, User.Dsl]"),
+        OuroborosDsl.Encoding("User.Dsl", "TemporalDiagrams.Mermaid", "HighlightEncoder[Mermaid, User.Dsl]"),
+        OuroborosDsl.Encoding("TemporalDiagrams.PlantUml", "Plantuml", "DiagramEncoder[Plantuml]"),
+        OuroborosDsl.Encoding("TemporalDiagrams.Mermaid", "Mermaid", "DiagramEncoder[Mermaid]")
+      )
+      .map(_.r)
 }
 
-class WriteOuroborosDiagram[F[_]: Applicative] {
+// TODO need left to write support
+class WriteOuroborosDiagram[F[_]](out: FilePrinterAlg[F]) {
   def run: F[Unit] =
-    Applicative[F].unit
+    out.print("ouroboros.puml")(
+      Renderable
+        .renderMany(WriteOuroborosDiagram.diagram)
+        .pipe(PlantUml.render(_))
+        .mkString_("\n")
+    )
 }
