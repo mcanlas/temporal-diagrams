9efa131b9c00fcf2669eebaa7f62e9107a25bfb7
Mark Canlas <github@htmlism.com>
2023-10-17 07:51:39 -0400

    start multi arrow suite

---
+++ temporal-diagrams-core/src/test/scala/com/htmlism/temporaldiagrams/v2/syntax/MultiArrowSuite.scala
@@ -0,0 +1,41 @@
+package com.htmlism.temporaldiagrams.v2.syntax
+
+import weaver.FunSuite
+
+object MultiArrowSuite extends FunSuite {
+  test("syntax specifically targets the simple enum") {
+    expect.eql(1, 1)
+  }
+
+  test("can add multi-arrow sources") {
+    expect.eql(1, 1)
+  }
+
+  test("can add multi-arrow destinations") {
+    expect.eql(1, 1)
+  }
+
+  test("can define multi-arrows") {
+    expect.eql(1, 1)
+  }
+
+  test("rendering multi-arrows is fallible given an undefined source") {
+    expect.eql(1, 1)
+  }
+
+  test("rendering multi-arrows is fallible given an undefined destination") {
+    expect.eql(1, 1)
+  }
+
+  test("rendering multi-arrows can be ignored") {
+    expect.eql(1, 1)
+  }
+
+  test("rendering multi-arrows becomes one domain language") {
+    expect.eql(1, 1)
+  }
+
+  test("collecting renderables gets rid of multi-arrow classes") {
+    expect.eql(1, 1)
+  }
+}
