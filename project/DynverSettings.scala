fb2b3e5ab953e0432145efce4a680291fb291fa5
Mark Canlas <github@htmlism.com>
2023-10-24 11:56:29 -0400

    correct ordering of version mutation

--- project/DynverSettings.scala
+++ project/DynverSettings.scala
@@ -3,7 +3,11 @@ import sbt.Keys.*
 import sbtdynver.DynVerPlugin.autoImport.*
 
 object DynverSettings extends AutoPlugin {
-  override def trigger: PluginTrigger = AllRequirements
+  override def trigger: PluginTrigger =
+    allRequirements
+
+  override def requires: Plugins =
+    sbtdynver.DynVerPlugin
 
   override def buildSettings: Seq[Setting[?]] =
     List(
