5820c1c457c747571491d83f31d741344695585a
Mark Canlas <github@htmlism.com>
2023-10-25 00:26:54 -0400

    yellow

--- temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/FraudEcosystemDsl.scala
+++ temporal-diagrams-demo/src/main/scala/com/htmlism/temporaldiagrams/demo/v2/FraudEcosystemDsl.scala
@@ -35,29 +35,77 @@ object FraudEcosystemDsl:
     def encodeBrightly(x: FraudEcosystemDsl, isBright: Boolean): Chain[PlantUml] =
       x match
         case EcsService(name) =>
-          Chain:
-            PlantUml.Component(name, None, Option.when(isBright)("ECS Service"))
+          Chain(
+            PlantUml.Component(name, None, Option.when(isBright)("ECS Service")),
+            if isBright then blue("component", "ECS Service") else white("component")
+          )
 
         case Lambda(name) =>
-          Chain:
-            PlantUml.Component(name, None, Option.when(isBright)("Lambda"))
+          Chain(
+            PlantUml.Component(name, None, Option.when(isBright)("Lambda")),
+            if isBright then red("component", "Lambda") else white("component")
+          )
 
         case Kinesis(name) =>
-          Chain:
-            PlantUml.Queue(name, None, Option.when(isBright)("Kinesis"))
+          Chain(
+            PlantUml.Queue(name, None, Option.when(isBright)("Kinesis")),
+            if isBright then red("queue", "Kinesis") else white("queue")
+          )
 
         case MySql(name) =>
-          Chain:
-            PlantUml.Database(name, None, Option.when(isBright)("MySQL"), Nil)
+          Chain(
+            PlantUml.Database(name, None, Option.when(isBright)("MySQL"), Nil),
+            if isBright then blue("database", "MySQL") else white("database")
+          )
 
         case DynamoDb(name) =>
-          Chain:
-            PlantUml.Database(name, None, Option.when(isBright)("DynamoDB"), Nil)
+          Chain(
+            PlantUml.Database(name, None, Option.when(isBright)("DynamoDB"), Nil),
+            if isBright then red("database", "DynamoDB") else white("database")
+          )
 
         case Flink(name) =>
-          Chain:
-            PlantUml.Component(name, None, Option.when(isBright)("Flink"))
+          Chain(
+            PlantUml.Component(name, None, Option.when(isBright)("Flink")),
+            if isBright then yellow("component", "Flink") else white("component")
+          )
 
         case Link(src, comment, dest) =>
           Chain:
             PlantUml.Arrow(src, dest, comment.some)
+
+  private def red(name: String, stereotype: String) =
+    PlantUml
+      .SkinParamGroup(name, stereotype)
+      .and("fontStyle", "bold")
+      .and("fontColor", "white")
+      .and("backgroundColor", "#bc4f4f")
+      .and("borderColor", "#642a2a")
+      .and("borderThickness", "2")
+
+  private def blue(name: String, stereotype: String) =
+    PlantUml
+      .SkinParamGroup(name, stereotype)
+      .and("fontStyle", "bold")
+      .and("fontColor", "white")
+      .and("backgroundColor", "#586ba4")
+      .and("borderColor", "#223336")
+      .and("borderThickness", "2")
+
+  private def yellow(name: String, stereotype: String) =
+    PlantUml
+      .SkinParamGroup(name, stereotype)
+      .and("fontStyle", "bold")
+      .and("fontColor", "#444")
+      .and("backgroundColor", "#EEDD82")
+      .and("borderColor", "#807746")
+      .and("borderThickness", "2")
+
+  private def white(name: String) =
+    PlantUml
+      .SkinParamGroup(name)
+      .and("fontStyle", "bold")
+      .and("fontColor", "#AAA")
+      .and("backgroundColor", "white")
+      .and("borderColor", "#AAA")
+      .and("borderThickness", "2")
