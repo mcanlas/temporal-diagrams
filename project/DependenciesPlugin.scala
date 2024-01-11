1870dae49ed4223fe960d81b430900d8f4e3f9c0
Mark Canlas <github@htmlism.com>
2024-01-11 07:30:10 -0500

    use dependencies plugin

---
+++ project/DependenciesPlugin.scala
@@ -0,0 +1,39 @@
+import sbt.Keys.*
+import sbt.*
+
+object DependenciesPlugin extends AutoPlugin {
+  override def trigger = allRequirements
+
+  object autoImport {
+    implicit class DependencyOps(p: Project) {
+      val http4sVersion =
+        "0.23.23"
+
+      def withCats: Project =
+        p
+          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.10.0")
+
+      def withHttpServer: Project =
+        p
+          .settings(
+            libraryDependencies ++= Seq(
+              "org.http4s" %% "http4s-dsl"          % http4sVersion,
+              "org.http4s" %% "http4s-blaze-server" % "0.23.15"
+            )
+          )
+
+      def withTesting: Project = {
+        val weaverVersion =
+          "0.8.3"
+
+        p.settings(
+          testFrameworks += new TestFramework("weaver.framework.CatsEffect"),
+          libraryDependencies ++= Seq(
+            "com.disneystreaming" %% "weaver-cats"       % weaverVersion % Test,
+            "com.disneystreaming" %% "weaver-scalacheck" % weaverVersion % Test
+          )
+        )
+      }
+    }
+  }
+}
