a2cfb3b1083375032958bd5c928927bb1c094c28
Mark Canlas <github@htmlism.com>
2024-01-25 16:27:13 -0500

    drop unused ops

--- temporal-diagrams-core/src/main/scala/com/htmlism/temporaldiagrams/syntax/package.scala
+++ temporal-diagrams-core/src/main/scala/com/htmlism/temporaldiagrams/syntax/package.scala
@@ -1,15 +1,6 @@
 package com.htmlism.temporaldiagrams
 package syntax
 
-import cats.data.*
-
 extension [A](x: A)
-  def iff(cond: Boolean, f: A => A): A =
-    if cond then f(x)
-    else x
-
   def list: List[A] =
     List(x)
-
-  def nel: NonEmptyList[A] =
-    NonEmptyList.one(x)
