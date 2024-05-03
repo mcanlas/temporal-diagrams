f3053c40f6a9d515c6aab597288a71ae4b9204c7
Mark Canlas <github@htmlism.com>
2024-05-03 12:51:44 -0400

    start test

--- temporal-diagrams-plantuml/src/main/scala/com/htmlism/temporaldiagrams/plantuml/sequence/SequenceDiagram.scala
+++ temporal-diagrams-plantuml/src/main/scala/com/htmlism/temporaldiagrams/plantuml/sequence/SequenceDiagram.scala
@@ -3,7 +3,7 @@ package com.htmlism.temporaldiagrams.plantuml.sequence
 import scala.collection.immutable.ListSet
 
 case class SequenceDiagram(
-    directives: Set[String],
-    participants: ListSet[Participant],
-    messages: List[Message]
+    directives: Set[String]            = Set.empty,
+    participants: ListSet[Participant] = ListSet.empty,
+    messages: List[Message]            = Nil
 )
--- temporal-diagrams-plantuml/src/test/scala/com/htmlism/temporaldiagrams/plantuml/sequence/SequenceDiagramSuite.scala
+++ temporal-diagrams-plantuml/src/test/scala/com/htmlism/temporaldiagrams/plantuml/sequence/SequenceDiagramSuite.scala
@@ -1,5 +1,21 @@
 package com.htmlism.temporaldiagrams.plantuml.sequence
 
+import scala.collection.immutable.ListSet
+
 import weaver.*
 
-object SequenceDiagramSuite extends FunSuite
+object SequenceDiagramSuite extends FunSuite:
+  test("PlantUML.com basic examples".ignore):
+    val _ =
+      SequenceDiagram(
+        participants = ListSet(
+          Participant("Alice"),
+          Participant("Bob")
+        ),
+        messages = List(
+          Message("Alice", "Bob"),
+          Message("Bob", "Alice")
+        )
+      )
+
+    expect.eql(1, 1)
