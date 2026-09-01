7d0c323a994529ec3404ea25c66d8b5fa52290bd
Mark Canlas <github@htmlism.com>
2026-09-01 16:55:30 -0400

    cats-effect 3.7.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val catsCore   = "2.13.0"
-  val catsEffect = "3.7.0"
+  val catsEffect = "3.7.1"
   val http4s     = "0.23.34"
-  val weaver     = "0.12.0"
+  val weaver     = "0.8.4"
 }
